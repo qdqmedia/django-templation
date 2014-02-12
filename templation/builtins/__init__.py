98e4d0a3a2f4476656cea575c6598a31f19c40ff
Enrique Paredes <enrique@iknite.com>
2014-02-12 16:49:32 +0100

    fix TypeError in templates/base:627 args_check

--- templation/builtins/__init__.py
+++ templation/builtins/__init__.py
@@ -17,12 +17,12 @@ base.builtins = []
 
 class exclusion_filter(object):
     def __init__(self, function):
-        self.function = function
+        self._decorated_function = function
 
     def __call__(self, *args, **kwargs):
         if thread_locals.resource:
-            raise TemplateSyntaxError('{} is not a valid tag/filter to use in templation'.format(self.function.__name__))
-        return self.function(*args, **kwargs)
+            raise TemplateSyntaxError('{} is not a valid tag/filter to use in templation'.format(self._decorated_function.__name__))
+        return self._decorated_function(*args, **kwargs)
 
 
 for k, v in BUILTIN_LIBRARIES.items():
