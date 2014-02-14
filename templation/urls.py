464d55d687a664a5ed7da4f7ddafce1f647d5efc
Enrique Paredes <enrique@iknite.com>
2014-02-14 14:35:03 +0100

    add templation url.templation_static to manage statics in dev stage

---
+++ templation/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import patterns, url
+from .settings import DAV_ROOT, DAV_STATIC_URL
+from .views import static_view
+
+
+def templation_static(**kwargs):
+    """
+    Helper function to return a URL pattern for serving files in debug mode.
+    Mostly cloned from django.conf.urls.static function.
+
+    from templation.urls import templation_static
+
+    urlpatterns = patterns('',
+        # ... the rest of your URLconf goes here ...
+    ) + templation_static(**kwargs)
+
+    """
+    kwargs.update({
+        'document_root': DAV_ROOT
+    })
+
+    return patterns('',
+        url(r'{}/(?P<resource_id>.*)/(?P<path>.*)$'.format(DAV_STATIC_URL.lstrip('/')),
+            static_view, kwargs=kwargs),
+    )
