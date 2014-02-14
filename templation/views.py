9acb730c412e25d9afc4199c0cf013336f7f7276
Enrique Paredes <enrique@iknite.com>
2014-02-14 14:34:31 +0100

    add static_view for statics in django

--- templation/views.py
+++ templation/views.py
@@ -3,6 +3,7 @@ Mixin to add in your CCBV to store the object.pk as resource to be fetched in th
 """
 from __future__ import absolute_import
 from .locals import thread_locals
+from django.views.static import serve
 
 
 class ResourceStoreMixin(object):
@@ -21,3 +22,11 @@ class ResourceStoreMixin(object):
         context = super(ResourceStoreMixin, self).dispatch(request, *args, **kwargs)
         thread_locals.resource = self.get_templation_object(**kwargs)
         return context
+
+
+def static_view(request, resource_id, path, document_root=None, show_indexes=False):
+    """
+    Composition to return the static in development
+    """
+    path = '/' + '/'.join((resource_id, 'static', path))
+    return serve(request, path, document_root, show_indexes)
